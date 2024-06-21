69d5fe18b837ae91fc03278de736a2fb122c2ac3
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2024-06-21 10:35:43 -0500

    update files

--- src/test/java/tennisondexter/liveProject/AddRemoveBasketTest.java
+++ src/test/java/tennisondexter/liveProject/AddRemoveBasketTest.java
@@ -1,17 +1,18 @@
 package tennisondexter.liveProject;
 
 import java.io.IOException;
+import java.time.Duration;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import base.BasePage;
-import pageObject.HomePage;
-import pageObject.ShopContentPanel;
-import pageObject.ShopHomePage;
-import pageObject.ShopProductPage;
+import pageObject.*;
 
 public class AddRemoveBasketTest extends BasePage{
 
@@ -30,40 +31,56 @@ public class AddRemoveBasketTest extends BasePage{
     
     @Test
     public void addRemoveItem() throws IOException, InterruptedException {
-    	//Go to Homepage
+        //Go to Homepage
         HomePage home = new HomePage(driver);
         //Close cookie banner
         home.getCookie().click();
         //click Test Store link
         home.getTestStoreLink().click();
-        
+
         ShopHomePage shopHome = new ShopHomePage(driver);
         //Select product
         shopHome.getProdOne().click();
-        
+
         // Add more steps as needed
         ShopProductPage shopProd = new ShopProductPage(driver);
         //Select size
         Select option = new Select(shopProd.getSizeOption());
         option.selectByVisibleText("M");
-        
+
         //increase qty
         shopProd.getQuantIncrease().click();
         //Click add to cart
         shopProd.getAddToCartBtn().click();
-        
+
         ShopContentPanel cPanel = new ShopContentPanel(driver);
         //click continue shopping
         Thread.sleep(3000);
         cPanel.getContinueShopBtn().click();
         Thread.sleep(3000);
-      //click home page
+        //click home page
         shopProd.getHomepageLink().click();
         Thread.sleep(3000);
         shopHome.getProdTwo().click();
         shopProd.getAddToCartBtn().click();
         cPanel.getCheckoutBtn().click();
         Thread.sleep(3000);
+
+
+        ShoppingCart cart = new ShoppingCart(driver);
+        cart.getDeleteItemTwo().click();
+
+
+//        cart.getTotalAmount().getText();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
+
+        // Click continue shopping
+        wait.until(ExpectedConditions.invisibilityOf(cart.getDeleteItemTwo()));
+        System.out.println(cart.getTotalAmount().getText());
+
+        //checking value matches 45.24
+        // AssertJUnit.assertEquals(cart.getTotalAmount().getText(), "$45.24");
+        Assert.assertEquals(cart.getTotalAmount().getText(), "$45.24");
     	
     }
 
